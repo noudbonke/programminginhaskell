651f85aa7eba6261598c4f762c63f5fc96de012f
Noud <noudbonke@gmail.com>
2023-08-06 01:08:29 +0200

    modified programming example for chapter 7 exercises

--- binstrtrans.hs
+++ binstrtrans.hs
@@ -12,19 +12,41 @@ int2bin n = n `mod` 2 : int2bin (n `div` 2)
 make8 :: [Bit] -> [Bit]
 make8 bits = take 8 (bits ++ repeat 0)
 
-encode :: String -> [Bit]
-encode = concat . map (make8 . int2bin . ord)
+--added for exercise 7
+parity :: [Bit] -> Bit
+parity bits = (sum bits) `mod` 2
 
-chop8 :: [Bit] -> [[Bit]]
-chop8 [] = []
-chop8 bits = take 8 bits : chop8 (drop 8 bits)
+--added for exercise 7
+addParity :: [Bit] -> [Bit]
+addParity bits = parity bits : bits 
+
+--added for exercise 7
+checkParity :: [Bit] -> [Bit]
+checkParity bits | head bits == parity (tail bits) = tail bits
+                 | otherwise = error "at least one bit corrupted during transmission"
+
+--modified for exercise 7
+encode :: String -> [Bit]
+encode = concat . map (addParity . make8 . int2bin . ord)
+
+--modified for exercise 7
+chop9 :: [Bit] -> [[Bit]]
+chop9 [] = []
+chop9 bits = take 9 bits : chop9 (drop 9 bits)
 
 decode :: [Bit] -> String
-decode = map (chr . bin2int) . chop8
+decode = map (chr . bin2int . checkParity) . chop9
 
 transmit :: String -> String
 transmit = decode . channel . encode
 
 channel :: [Bit] -> [Bit]
 channel = id
+
+--added for exercise 8
+faultyChannel :: [Bit] -> [Bit]
+faultyChannel = tail
+
+faultyTransmit :: String -> String 
+faultyTransmit = decode . faultyChannel . encode
 

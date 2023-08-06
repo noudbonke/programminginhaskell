70c40f504fcc0bcf9a52732b8133da1347f8b2bd
Noud <noudbonke@gmail.com>
2023-08-06 18:29:01 +0200

    added chapter 8 exercise 8

--- tautologychecker.hs
+++ tautologychecker.hs
@@ -1,8 +1,11 @@
+--modified for exercise 8
 data Prop = Const Bool
           | Var Char
           | Not Prop
           | And Prop Prop
+          | Or Prop Prop
           | Imply Prop Prop
+          | Equiv Prop Prop
 
 type Assoc k v = [(k, v)]
 type Subst = Assoc Char Bool 
@@ -10,19 +13,25 @@ type Subst = Assoc Char Bool
 find :: Eq k => k -> Assoc k v -> v
 find k t = head [v | (k', v) <- t, k == k']
 
+--modified for exercise 8
 eval :: Subst -> Prop -> Bool
 eval _ (Const b) = b
 eval s (Var x) = find x s
 eval s (Not p) = not (eval s p)
 eval s (And p q) = eval s p && eval s q
+eval s (Or p q) = eval s p || eval s q
 eval s (Imply p q) = eval s p <= eval s q
+eval s (Equiv p q) = eval s p == eval s q 
 
+--modified for exercise 8
 vars :: Prop -> [Char]
 vars (Const _) = []
 vars (Var x) = [x]
 vars (Not p) = vars p
 vars (And p q) = vars p ++ vars q
+vars (Or p q) = vars p ++ vars q
 vars (Imply p q) = vars p ++ vars q
+vars (Equiv p q) = vars p ++ vars q
 
 bools :: Int -> [[Bool]]
 bools 0 = [[]]
@@ -52,3 +61,9 @@ p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
 
 p4 :: Prop
 p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')       
+
+p5 :: Prop 
+p5 = Equiv (Var 'A') (Var 'A')
+
+p6 :: Prop
+p6 = Imply (And (Not (Var 'A')) (Or (Var 'A') (Var 'B'))) (Var 'B') 
